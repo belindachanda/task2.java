66f859cf75f0aefa046f81e1977074764007b2cc
Belinda chanda <btatend1@stu.vistula.edu.pl>
2022-05-28 23:03:23 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {carAttri(String model, String brand, int year, int price, String color, int quantity) {
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.quantity = quantity;
+    }
+
+        public String getModel() {
+            return model;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public String getBrand() {
+            return brand;
+        }
+	// write your code here
+    }
+}
